4658c86f07fec53820baeab6a6a394b025d26a5a
TØP BØ¥ <nwankwogoodluck156@gmail.com>
2025-07-31 09:21:30 +0100

    Update multiBot.js

--- multiBot.js
+++ multiBot.js
@@ -77,7 +77,7 @@ async function send(bot) {
       }
     }
 
-    await new Promise(res => setTimeout(res, 1000)); // 1s between retries
+    await new Promise(res => setTimeout(res, 300)); // 1s between retries
   }
 }
 
